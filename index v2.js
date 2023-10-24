84bb1c5ee7dbc90d139dcf0356380d9cfeab8f44
jlkansakar <joka0002@stud.kea.dk>
2023-10-24 19:02:18 +0200

    actual work on project :3

--- index v2.js
+++ index v2.js
@@ -1,1 +1,45 @@
-const audioContext = new (window.AudioContext || window.webkitURL)()
+const audioContext = new (window.AudioContext || window.webkitAudioContext)()
+const activeOscillators = {}
+const waveformSelector = document.querySelector("#waveform")
+const getSelectedWaveform = () => waveformSelector.value
+waveformSelector.addEventListener("change", function() {
+    selectedWaveform = getSelectedWaveform();
+});
+
+let selectedWaveform = getSelectedWaveform()
+
+const createOscillatorNode = frequency => {
+    const oscillator = audioContext.createOscillator()
+    const gainNode = audioContext.createGain()
+
+    oscillator.type = selectedWaveform
+    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime)
+
+    oscillator.connect(gainNode)
+    gainNode.connect(audioContext.destination)
+
+    gainNode.gain.setValueAtTime(0, audioContext.currentTime)
+    gainNode.gain.linearRampToValueAtTime(1, audioContext.currentTime + 0.02)
+
+    oscillator.start()
+    return {oscillator, gainNode}
+}
+
+const playSound = frequency => {
+    if (!activeOscillators[frequency]) {
+        activeOscillators[frequency] = createOscillatorNode(frequency)
+    }
+}
+
+
+const stopSound = frequency => {
+    if (activeOscillators[frequency]) {
+        const {oscillator, gainNode} = activeOscillators[frequency]
+
+        gainNode.gain.setValueAtTime(gainNode.gain.value, audioContext.currentTime)
+        gainNode.gain.linearRampToValueAtTime(0, audioContext.currentTime + 0.02)
+
+        oscillator.stop(audioContext.currentTime + 0.02)
+        delete activeOscillators[frequency]
+    }
+}
