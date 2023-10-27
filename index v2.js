fab92e893171b5a19047ebd483b807687ed7917b
jlkansakar <joka0002@stud.kea.dk>
2023-10-27 11:58:46 +0200

    final touches - I FUCKING DID IT AHAHAHHAAHAHAHAHA

--- index v2.js
+++ index v2.js
@@ -1,18 +1,23 @@
+// laver min audiocontext - audioContext bruges til at skabe og spille al lyd fra en hjemmeside. Den bruger nodes til at fungere.
 let audioContext;
 window.onload = function () {
     audioContext = new (window.AudioContext)
 }
+
 const activeOscillators = {}
+
+// hiver fat i mine waveforms, som bruges til at ændre lydbølgen der bliver afspillet
 const waveformSelector = document.querySelector("#waveform")
 const getSelectedWaveform = () => waveformSelector.value
 let selectedWaveform = getSelectedWaveform()
-console.log(selectedWaveform)
+
+// laver en eventListener med typen "change", som gør at når man vælger fra dropdown menu'en, at min selectedWaveform bliver ændret.
 waveformSelector.addEventListener("change", function() {
     selectedWaveform = getSelectedWaveform();
 });
 
 
-
+// laver en node. Der bruges kommandoer fra audioContext.
 const createOscillatorNode = frequency => {
     const oscillator = audioContext.createOscillator()
     const gainNode = audioContext.createGain()
@@ -30,13 +35,14 @@ const createOscillatorNode = frequency => {
     return {oscillator, gainNode}
 }
 
+// essentially - hvis lyd != true, så lav en node med lyd.
 const playSound = frequency => {
     if (!activeOscillators[frequency]) {
         activeOscillators[frequency] = createOscillatorNode(frequency)
     }
 }
 
-
+// hvis lyd = true, slet den aktive oscillator.
 const stopSound = frequency => {
     if (activeOscillators[frequency]) {
         const {oscillator, gainNode} = activeOscillators[frequency]
@@ -48,7 +54,7 @@ const stopSound = frequency => {
         delete activeOscillators[frequency]
     }
 }
-
+// opsætning af hvordan man spiller synth'en - mousedown betyder at klikke og holde musen nede, mouseup betyder du stopper med at klikke, mouseleave betyder du fjerner musen.
 const setupButtonListeners = () => {
     const noteButtons = document.querySelectorAll(".note")
     noteButtons.forEach((button, index) => {
@@ -59,5 +65,5 @@ const setupButtonListeners = () => {
 
     })
 }
-
+// kører funktionen
 setupButtonListeners()
